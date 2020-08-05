53227b9f39f9382e60bb368a597f3545b8d0fc2e
prestf8 <prestf8@gmail.com>
2020-08-04 21:12:43 -0700

    Remove directory

--- Front-end/No-Framework/giannis-antetokounmpo-website/script-gallery.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("logo").style.width = "50px";
-document.querySelector(".milwaukee-container").style.marginTop = "0";
-document.getElementById("milwaukee-header").style.height = "8vh";
--- Front-end/No-Framework/kaggle-website-unfinished/scripts/header.js
+++
@@ -1,8 +0,0 @@
-window.onscroll = function() {
-    var currentScrollPos = window.pageYOffset;
-      if (currentScrollPos > 10) {
-        document.getElementById("header").style.top = "-80px";
-      } else {
-        document.getElementById("header").style.top = "0";
-      }
-}
